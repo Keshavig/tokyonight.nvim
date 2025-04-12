ac155f68fd27ae532bf311e9c2b101e900d056f2
Keshavig <keshavbhatta222@gmail.com>
2025-04-12 15:57:44 +0545

    Changed bg_night to #282c34

--- lua/tokyonight/colors/night.lua
+++ lua/tokyonight/colors/night.lua
@@ -2,10 +2,10 @@ local ret = vim.deepcopy(require("tokyonight.colors.storm"))
 
 ---@type Palette
 return vim.tbl_deep_extend("force", ret, {
-  -- bg = "#1a1b26",
-  -- bg_dark = "#16161e",
-  
-  bg = "#16161e",
-  bg_dark = "#1a1b26",
+  bg = "#1a1b26",
+  bg_dark = "#282c34",
+
+  -- bg = "#16161e",
+  -- bg_dark = "#1a1b26",
   bg_dark1 = "#0C0E14",
 })
